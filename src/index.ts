f4c17cacb86141be6cb9e6e8fe56e89a27a8e4c6
Vic <74506415+v1ctorio@users.noreply.github.com>
2024-08-11 19:12:17 +0200

    bun bot

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,25 @@
 import { App } from "@slack/bolt";
 
-const { SLACK_BOT_TOKEN, SLACK_SIGNING_SECRET } = process.env;
+const { SLACK_BOT_TOKEN, SLACK_SIGNING_SECRET, SLACK_APP_TOKEN } = Bun.env;
+
+const HACK_NIGHT_CHANNEL = "C07GCBZPEJ1";
+
+//console.log( { SLACK_BOT_TOKEN, SLACK_SIGNING_SECRET });
 
 const app = new App({
-	token: SLACK_BOT_TOKEN,
+    token: SLACK_BOT_TOKEN,
+	appToken: SLACK_APP_TOKEN,
+    socketMode: true,
 	signingSecret: SLACK_SIGNING_SECRET,
 });
 
-await app.start(process.env.PORT || 3000);
+await app.start();
 console.log("Bolt slack bot working");
+
+
+app.message("hacknighttest", async ({ message, say }) => {
+    //if (message.subtype !== "thread_broadcast") return;
+    if (message.channel !== HACK_NIGHT_CHANNEL) return; // ONly respond to the HACK NIGHT CHANNEl
+    console.log("Message received",message);
+    await say(`Hello, <@${''}>! ${message.subtype}`);
+});
